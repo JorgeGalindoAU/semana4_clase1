a4976e642c1d9f8eecf051da949e27ec54d99de3
Jorge Galindo <jorge.galindo.zetes@gmail.com>
2025-06-25 16:22:43 +0200

    Created new components for adding the create form

---
+++ src/screens/create-user-screen/create-user-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { GoBackButtonComponent } from "../../components/go-back-button/go-back-button.component";
+
+@Component({
+  selector: 'app-create-user-screen',
+  imports: [GoBackButtonComponent],
+  templateUrl: './create-user-screen.component.html',
+  styleUrl: './create-user-screen.component.css'
+})
+export class CreateUserScreenComponent {
+
+}
--- src/screens/main-screen/main-screen.component.ts
+++ src/screens/main-screen/main-screen.component.ts
@@ -6,24 +6,17 @@ import { UserService } from '../../services/user.service';
 import { IUser } from '../../interfaces/user.interface';
 import { Pagination } from "../../components/pagination/pagination.component";
 import { IUserPagination } from '../../interfaces/user_pagination.interface';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-main-screen',
-  imports: [TitleComponent, ButtonComponent, ActiveFilterComponent, Pagination],
+  imports: [TitleComponent, ButtonComponent, RouterLink],
   templateUrl: './main-screen.component.html',
   styleUrl: './main-screen.component.css',
   standalone: true,
 })
 export class MainScreenComponent implements OnInit, OnDestroy {
-  private userService: UserService = inject(UserService);
 
-  users = signal<IUser[]>([]);
-  currentPage = signal<number>(1);
-  currentCount = signal<number>(1);
-  totalPages = signal<number>(1);
-
-  showInactive = signal<boolean>(false);
-  showExternals = signal<boolean>(false);
 
   constructor() {
     // inicializar las variables
@@ -33,7 +26,6 @@ export class MainScreenComponent implements OnInit, OnDestroy {
   ngOnInit(): void {
     // mostrar un loading
     // llamar a nuestra API por HTTP
-    this.getUsers();
   }
 
   ngOnDestroy(): void {
@@ -48,40 +40,5 @@ export class MainScreenComponent implements OnInit, OnDestroy {
   }
   configure() {
     console.log("Configure!");
-  }
-
-  showInactiveUsers(value: boolean) {
-    this.showInactive.set(value);
-  }
-
-  showExternalUsers(value: boolean) {
-    this.showExternals.set(value);
-  }
-
-  receivedNewPage(page: number) {
-    this.currentPage.set(page);
-
-    this.getUsers();
-  }
-
-  receivedNewCount(count: number) {
-    this.currentCount.set(count);
-
-    this.getUsers();
-  }
-
-  getUsers() {
-    this.userService.getUsers(this.currentCount(), this.currentPage())
-      .subscribe(
-        {
-          next: (value: IUserPagination) => {
-            this.totalPages.set(value.total_pages);
-
-            let users: IUser[] = value.data;
-            this.users.set(users);
-          },
-          error: (err) => console.error(err), // NOK
-        }
-      );
   }
 }
